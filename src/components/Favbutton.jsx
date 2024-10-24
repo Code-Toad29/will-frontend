23c31a8a5acbf3a9400182cec3e27a2d9d4041c2
Code-Toad29 <princetyrique52@gmail.com>
2024-10-24 15:01:07 +0200

    updated stuff

--- src/components/Favbutton.jsx
+++ src/components/Favbutton.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import { FaHeart } from 'react-icons/fa';
+import React from "react";
+import { FaHeart } from "react-icons/fa";
 
 const FavButton = () => {
   return (
-    <FaHeart 
-      size={23} 
-      className='bg-red-500 cursor-pointer drop-shadow-sm' 
-    />
+    <FaHeart size={23} className="bg-red-500 cursor-pointer drop-shadow-sm" />
   );
-}
+};
 
 export default FavButton;
--- src/components/ItemSec.jsx
+++ src/components/ItemSec.jsx
@@ -96,47 +96,51 @@
 // safe
 // import React from "react";
 // import FavButton from "./FavButton";
-// import { MdOutlineBathtub, MdOutlineBed, MdOutlineGarage } from "react-icons/md";
+// import {
+//   MdOutlineBathtub,
+//   MdOutlineBed,
+//   MdOutlineGarage,
+// } from "react-icons/md";
 // import { Link } from "react-router-dom";
 
 // const Item = ({ property }) => {
-//     return (
-//         <div>
-//             <div className="rounded-2xl p-5 bg-white">
-//                 <img src={property.image} alt={property .title} className="rounded" />
-//                 {/* Heart Button */}
-//                 <FavButton />
-//             </div>
-//             <div>
-//                 <h5>{property.city}</h5>
-//                 <h4 className="medium-18 line-clamp">{property.title}</h4>
-//             </div>
-//             {/* Info */}
-//             <div className="flex gap-x-2 py-2">
-//                 <div className="flex items-center gap-x-2 border-r border-slate-900/50 pr-4 font-[500]">
-//                     <MdOutlineBed />
-//                     {property.facilities.bedrooms}
-//                 </div>
-//                 <div className="flex items-center gap-x-2 border-r border-slate-900/50 pr-4 font-[500]">
-//                     <MdOutlineBathtub />
-//                     {property.facilities.bathrooms}
-//                 </div>
-//                 <div className="flex items-center gap-x-2 border-r border-slate-900/50 pr-4 font-[500]">
-//                     <MdOutlineGarage />
-//                     {property.facilities.parking}
-//                 </div>
-//             </div>
-//             <p>{property.description}</p>
-//             <div className="flex justify-between">
-//                 <div>${property.price}.00</div>
-//                 <Link to="/">
-//                     <button className="btn-secondary roundedx1 !py-[7px] !px-5 shadow-sm">
-//                         View Details
-//                     </button>
-//                 </Link>
-//             </div>
+//   return (
+//     <div>
+//       <div className="rounded-2xl p-5 bg-white">
+//         <img src={property.image} alt={property.title} className="rounded" />
+//         {/* Heart Button */}
+//         <FavButton />
+//       </div>
+//       <div>
+//         <h5>{property.city}</h5>
+//         <h4 className="medium-18 line-clamp">{property.title}</h4>
+//       </div>
+//       {/* Info */}
+//       <div className="flex gap-x-2 py-2">
+//         <div className="flex items-center gap-x-2 border-r border-slate-900/50 pr-4 font-[500]">
+//           <MdOutlineBed />
+//           {property.facilities.bedrooms}
 //         </div>
-//     );
+//         <div className="flex items-center gap-x-2 border-r border-slate-900/50 pr-4 font-[500]">
+//           <MdOutlineBathtub />
+//           {property.facilities.bathrooms}
+//         </div>
+//         <div className="flex items-center gap-x-2 border-r border-slate-900/50 pr-4 font-[500]">
+//           <MdOutlineGarage />
+//           {property.facilities.parking}
+//         </div>
+//       </div>
+//       <p>{property.description}</p>
+//       <div className="flex justify-between">
+//         <div>${property.price}.00</div>
+//         <Link to="/">
+//           <button className="btn-secondary roundedx1 !py-[7px] !px-5 shadow-sm">
+//             View Details
+//           </button>
+//         </Link>
+//       </div>
+//     </div>
+//   );
 // };
 
 // export default Item;
