1a4350c4dc9ae5d456f01d2e49c18bb3c62e6ef2
Code-Toad29 <princetyrique52@gmail.com>
2024-10-29 19:01:54 +0200

    free

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
-import "./Companies.css"
+// import React from 'react'
+// import "./Companies.css"
 
-const  Companies =() => {
-return (
-    <section className='c-wrappper'>
-    <div className='paddings  innerWidth flexCenter c-container'>
-    <img src='./Jawitz.png'  alt=''/>
-    <img src='./Remax.png' alt=''/>
-    <img src='./PamGolding.png' alt=''/>
-    <img src='./Stanlib.png' alt=''/>
-    </div>
+// const  Companies =() => {
+// return (
+//     <section className='c-wrappper'>
+//     <div className='paddings  innerWidth flexCenter c-container'>
+//     <img src='./Jawitz.png'  alt=''/>
+//     <img src='./Remax.png' alt=''/>
+//     <img src='./PamGolding.png' alt=''/>
+//     <img src='./Stanlib.png' alt=''/>
+//     </div>
+//     </section>
+
+// )
+// };
+
+// export default Companies;
+
+// to
+import React from "react";
+import "./Companies.css";
+
+const Companies = () => {
+  return (
+    <section className="c-wrapper">
+      <div className="paddings innerWidth flexCenter c-container">
+        <img src="./Jawitz.png" alt="Jawitz" />
+        <img src="./Remax.png" alt="Remax" />
+        <img src="./PamGolding.png" alt="Pam Golding" />
+        <img src="./Stanlib.png" alt="Stanlib" />
+      </div>
     </section>
-    
-    
-)
+  );
 };
-
 
 export default Companies;
